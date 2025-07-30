85e78d53edf37534d0bc8129fa0cc10b55ddacb2
hanseok <hanseok@ihopper.co.kr>
2025-07-30 16:54:35 +0900

    유저생성시 point 데이터 생성 기능 업데이트

--- E-commerce/src/main/java/com/example/ecommerce/user/application/facade/UserFacade.java
+++ E-commerce/src/main/java/com/example/ecommerce/user/application/facade/UserFacade.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.user.application.facade;
 
+import com.example.ecommerce.point.domain.service.PointService;
 import com.example.ecommerce.user.domain.model.User;
 import com.example.ecommerce.user.domain.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -7,17 +8,23 @@ import org.springframework.stereotype.Component;
 
 import com.example.ecommerce.user.application.dto.UserCreateRequest;
 import com.example.ecommerce.user.application.dto.UserCreateResponse;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class UserFacade {
+    private UserService userService;
+    private PointService pointService;
 
-    @Autowired
-    UserService userService;
+    public UserFacade(UserService userService, PointService pointService) {
+        this.userService = userService;
+        this.pointService = pointService;
+    }
 
+    @Transactional
     public UserCreateResponse createUserWithInitialPoint(UserCreateRequest request) {
         User user = userService.create(request); // 1. 유저 생성
         // 유저 생성시 포인트 정보 생성
-        // pointService.recordJoinBonus(user, 0L); // 2. 초기 포인트 기록
+        pointService.recordJoinBonus(user); // 2. 초기 포인트 기록
         return UserCreateResponse.from(user);
     }
 }
